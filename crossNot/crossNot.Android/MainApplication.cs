ea1d7450b53b177ed03c46e09d10f4f526510698
FabriGinseng <antoniofabriziofiume@libero.it>
2017-11-29 12:33:05 +0100

    commit iniziale

---
+++ crossNot/crossNot.Android/MainApplication.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Plugin.FirebasePushNotification;
+
+namespace crossNot.Droid
+{
+    [Application]
+    public class MainApplication : Application
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            FirebasePushNotificationManager.Initialize(this, true);
+#else
+              FirebasePushNotificationManager.Initialize(this,false);
+#endif
+
+            //Handle notification when app is closed here
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+
+
+            };
+        }
+    }
+}
